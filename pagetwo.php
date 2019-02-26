ead343405b5c2eff77b6fa5a6947a50fc5a9bae7
chloeod95 <46936902+chloeod95@users.noreply.github.com>
2019-02-26 19:49:20 +0000

    other

--- pagetwo.php
+++ pagetwo.php
@@ -13,9 +13,9 @@ $cust_email = htmlentities($_POST['cust_email']);
 
 Address: <INPUT TYPE = "text" SIZE="50"name="cust_address"><br/>
 Phone: <INPUT TYPE = "text" SIZE="20"name="cust_phone"><br/>
-<INPUT TYPE="hidden"name name = "cust_name"
+<INPUT TYPE="hidden" name = "cust_name"
 VALUE =" <?php echo $cust_name;?>">
-<INPUT TYPE ="Hidden" name="cust_email"
+<INPUT TYPE ="hidden" name="cust_email"
 VALUE = "<?php echo $cust_email;?>"
 <INPUT TYPE="submit" name="submit2"value="Proceed">
 
